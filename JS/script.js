106056584bf9db94aa75c60b64a838bfa486c44a
Angel Luciano Ciancio <101909053+AngelLC18@users.noreply.github.com>
2022-10-07 23:31:43 -0300

    Añadiendo evento de sumatoria random para el captcha

--- JS/script.js
+++ JS/script.js
@@ -1,27 +1,29 @@
 //#region Variables
-let input_val = document.getElementById('input_val');
+let valor_input_val = document.getElementById('valor_input');
 let num1 = document.getElementById('num1');
 let num2 = document.getElementById('num2');
 const myBtn = document.getElementById('btn');
-let number1 = num1.innerText;
-let number2 = num2.innerText;
 //#endregion
 
 //#region Calculo Matematico que me tire un número random
 //retorna un entero aleatorio del 0 al 9
-num1.innerText = Math.floor(Math.random() * 10);
-num2.innerText = Math.floor(Math.random() * 10);
-//#endregion
-//#region Evento
+num1.innerText = Math.floor(Math.random() * 10)
+num2.innerText = Math.floor(Math.random() * 10)
+    //#endregion
+    //#region Evento
+let number1 = num1.innerText;
+let number2 = num2.innerText;
 myBtn.addEventListener("click", () => {
     let sum_result = parseInt(number1) + parseInt(number2);
-    let res = parseInt(input_val.value)
+    let res = parseInt(valor_input.value)
     if (res == sum_result) {
         alert("Usted no es un robot");
-        document.getElementsByClassName('preguntas').style.display = 'none';
-        document.getElementsByClassName('captcha').style.display = 'none';
+        document.getElementById('parent').style.display = 'block';
+        document.getElementById('captcha').style.display = 'none';
+
     } else {
         alert("Usted es un robot");
+
     }
 });
 //#endregion
